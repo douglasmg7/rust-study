8773a87e9e8a8b40b6330aac65f895060668bdf5
douglasmg7 <douglasmg7@gmail.com>
2024-05-02 10:49:38 -0300

    using new rand version

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     // println!("The secret number is: {}", secret_number);
 
@@ -18,12 +18,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Fail to read line");
 
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
