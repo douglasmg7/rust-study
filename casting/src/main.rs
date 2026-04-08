5eacb9d87370e4e566024d450b9c8b2cb34a5824
douglasmg7 <douglasmg7@gmail.com>
2026-04-08 15:45:12 -0300

    Casting

---
+++ casting/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let distance: i32 = 50;
+    let dis: i8 = distance as i8;
+    println!("dis: {dis}");
+
+    let dis_f: f64 = 345.178;
+    let dis_d: i32 = dis_f.ceil() as i32;
+    println!("dis_d: {dis_d}");
+}
