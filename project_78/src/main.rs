17479c0c9089264e4124de7b1ce522ad23ff2588
douglasmg7 <douglasmg7@gmail.com>
2026-04-09 19:49:12 -0300

    Poject 78

---
+++ project_78/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    apply_to_jobs(35, "Rust Developer");
+    println!("{}", is_even(8));
+    println!("{}", is_even(9));
+
+    println!("{:?}", alphabets("aardvark")); // -> (true, false)
+    println!("{:?}", alphabets("zoology")); // -> (false, true)
+    println!("{:?}", alphabets("zebra")); //-> (true, true)
+}
+
+fn apply_to_jobs(number: i32, title: &str) {
+    println!("I'm applying to {number} {title} jobs");
+}
+
+fn is_even(number: i32) -> bool {
+    number % 2 == 0
+}
+
+fn alphabets(text: &str) -> (bool, bool) {
+    (text.contains('a'), text.contains('z'))
+}
+
+// https://doc.rust-lang.org/std/primitive.str.html#method.contains
