06778c649c85acef6edc2e0a9e547d78b22de482
douglasmg7 <douglasmg7@gmail.com>
2026-08-07 08:23:19 -0300

    Using lifetime when there is two ref parameters.

--- lifetimes_and_referents/src/main.rs
+++ lifetimes_and_referents/src/main.rs
@@ -2,6 +2,19 @@ fn select_two_first_cities<'a>(items: &'a [String]) -> &'a [String] {
     &items[0..2]
 }
 
+fn choose_favorite<'a>(first: &'a str, second: &str) -> &'a str {
+    println!("{second}");
+    first
+}
+
+fn choose_longest<'a>(first: &'a str, second: &'a str) -> &'a str {
+    if first.len() > second.len() {
+        first
+    } else {
+        second
+    }
+}
+
 fn main() {
     let cities = vec![
         "Barcelona".to_string(),
@@ -11,4 +24,12 @@ fn main() {
 
     let two_cities = select_two_first_cities(&cities);
     println!("Cities: {:?}", two_cities);
+
+    println!("Returned vale: {:?}", choose_favorite("aaa", "bbb"));
+
+    let orlando = "Orlando".to_string();
+    let north_caroline = "North Caroline".to_string();
+
+    let longest_one = choose_longest(&orlando, &north_caroline);
+    println!("Longest value : {:?}", longest_one);
 }
